45216c57dcf18dd219ba34fee4cf0c5375206997
fathsam82 <fathsam82@gmail.com>
2023-05-26 16:55:45 -0600

    README created

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+	private FoodTruck [] fleet;
+	public static void main(String[] args) {
+		FoodTruckApp app = new FoodTruckApp();
+		app.run();
+
+	}
+	public void run() {
+	//	another();
+	//}
+	//public void another() {
+		
+	//}
+
+//}
